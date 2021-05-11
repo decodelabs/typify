c8f1410f070394a3558be426c2bdfdb2332aa0c0
Tom Wright <tom@inflatablecookie.com>
2021-05-11 11:38:55 +0000

    Added Veneer IDE support stub
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ stubs/Typify.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This is a stub file for IDE compatibility only.
+ * It should not be included in your projects.
+ */
+namespace DecodeLabs;
+use DecodeLabs\Veneer\Proxy;
+use DecodeLabs\Veneer\ProxyTrait;
+use DecodeLabs\Typify\Detector as Inst;
+class Typify implements Proxy { use ProxyTrait; 
+const VENEER = 'Typify';
+const VENEER_TARGET = Inst::class;};
